9252392092e5c88353f1b00c4c8dd35f428ae9e7
Pete Melgren <pmelgren@gmail.com>
2020-03-26 10:22:31 -0400

    Add notebook, and query to generate a sample heat map. Ignore jpegs.

---
+++ prediction_data.sql
@@ -0,0 +1,37 @@
+select 
+	bat_side
+	,pitch_side
+	,balls
+	,strikes
+	,velocity
+	,spin_rate
+	,break_x
+	,break_z
+	,release_x
+	,release_z
+	,extension
+	,plate_x
+	,plate_z
+	,sz_top
+	,sz_bot
+	,vx50
+	,vy50
+	,vz50
+	,ax50
+	,ay50
+	,az50
+	,if_alignment
+	,of_alignment
+from 
+	tm.play play
+	inner join tm.pitch pit
+		on pit.play_id = play.play_id
+	inner join tm.pitch_measures pm
+		on pm.pitch_id = pit.pitch_id
+	inner join tm.play_defense def
+		on def.play_id = play.play_id
+where
+	play.pitcher_id = {}
+	and pit.pitch_type = '{}'
+	and bat_side = '{}'
+	and date_part('year',pitch_date) = 2019
